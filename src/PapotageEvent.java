89a2c2cece84ee98f9abaadcf5acbbbcd13984e6
Trikzy7 <theoplebani2002@icloud.com>
2023-04-28 09:59:50 +0200

    Init

---
+++ src/PapotageEvent.java
@@ -0,0 +1,20 @@
+import java.util.EventObject;
+
+public class PapotageEvent extends EventObject {
+    private String sujet;
+    private String corps;
+
+    public String getSujet() {
+        return sujet;
+    }
+
+    public String getCorps() {
+        return corps;
+    }
+
+    public PapotageEvent(Object source, String sujet, String corps) {
+        super(source);
+        this.sujet = sujet;
+        this.corps = corps;
+    }
+}
